a8d702d8540e22344fd6aef0099e72154d8e9157
hurtstotouchfire <hurtstotouchfire@gmail.com>
2013-09-28 13:13:58 -0400

    rough first install script, emacs.d instead of .emacs

--- install.sh
+++ install.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
+#run once
 
 dir=~/src/dot
 olddir=~/dotfiles_old
 mkdir $olddir
+
+files="bashrc bash_aliases emacs.d"
+
+for file in $files; do
+    mv ~/.$file $olddir
+    ln -s $dir/$file ~/.$file
+done
+
+source ~/.bashrc
+
