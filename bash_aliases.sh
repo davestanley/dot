ae843807e8774142c132f2b39d73db5b8ccca45a
hurtstotouchfire <hurtstotouchfire@gmail.com>
2013-09-28 13:48:05 -0400

    cleaning up aliases

--- bash_aliases.sh
+++ bash_aliases.sh
@@ -3,15 +3,14 @@ alias emacs="emacs -nw"
 alias src="source ~/.bashrc &> /dev/null; source ~/.bash_profile &> /dev/null"
 
 #ssh
-alias sshfs_61="sshfs kelly@knight61osx.dyn.berkeley.edu: /home/httf/knight61osx/"
 alias ssh_neuro="ssh kellyb@ssh.neuro.berkeley.edu"
-alias ssh_61="ssh kelly@knight61osx.dyn.berkeley.edu"
-alias ssh_knightlab="ssh kellybuchanan@knightlab.berkeley.edu"
 
 #sshfs
-alias sshfs_neuro="sshfs -o follow_symlinks kellyb@macfuse.neuro.berkeley.edu: /home/httf/HWNI/"
+#need to make this more cross-computer friendly
+#alias sshfs_neuro="sshfs -o follow_symlinks kellyb@macfuse.neuro.berkeley.edu: /home/httf/HWNI/"
 
 #linux aliases
+#pipe to for clipboard
 alias clip="xclip -sel clip"
 
 #mac aliases
