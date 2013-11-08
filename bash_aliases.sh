ed71552032418108c41a5b87a5ec107a717cf913
hurtstotouchfire <hurtstotouchfire@gmail.com>
2013-11-08 13:21:58 -0800

    adding hydra ssh alias

--- bash_aliases.sh
+++ bash_aliases.sh
@@ -2,8 +2,10 @@ alias emacs-w="/usr/bin/emacs"
 alias emacs="emacs -nw"
 alias src="source ~/.bashrc &> /dev/null; source ~/.bash_profile &> /dev/null"
 alias lt="ls -lt | head"
+alias untar="~/src/untar.sh"
 #ssh
 alias ssh_neuro="ssh kellyb@ssh.neuro.berkeley.edu"
+alias ssh_hydra="sh ~/.hydra.sh"
 
 #sshfs
 #need to make this more cross-computer friendly
