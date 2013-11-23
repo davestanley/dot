92302d4f61a7c5aa4f1c036c09e87e3109db0792
Kelly Buchanan <kellybuchanan@berkeley.edu>
2013-11-23 11:04:50 -0800

    moving global git ignore to normal setup script as symlink

--- setup.sh
+++ setup.sh
@@ -9,6 +9,11 @@ for file in $files; do
     ln -s $dir/$file ~/.$file
 done
 
+#softlink git ignore
+mkdir ~/.config
+mkdir ~/.config/git
+ln -s $dir/gitignore_global ~/.config/git/ignore
+
 # executing this script does not re-source bashrc properly, but running this one line manually in CL does.
 source ~/.bashrc
 
