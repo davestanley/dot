6a5ac859f347a143ad0c6fe72b4f6986529785b0
hurtstotouchfire <hurtstotouchfire@gmail.com>
2013-11-27 16:44:32 -0500

    suppress file exists errors

--- setup.sh
+++ setup.sh
@@ -6,13 +6,13 @@ dir=~/src/dot
 #softlink dotfiles
 files="bashrc bash_aliases.sh"
 for file in $files; do
-    ln -s $dir/$file ~/.$file
+    ln -s $dir/$file ~/.$file 2> /dev/null
 done
 
 #softlink git ignore
-mkdir ~/.config
-mkdir ~/.config/git
-ln -s $dir/gitignore_global ~/.config/git/ignore
+mkdir ~/.config 2> /dev/null
+mkdir ~/.config/git 2> /dev/null
+ln -s $dir/gitignore_global ~/.config/git/ignore 2> /dev/null
 
 # executing this script does not re-source bashrc properly, but running this one line manually in CL does.
 source ~/.bashrc
