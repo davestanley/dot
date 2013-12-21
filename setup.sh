21a41cf27bbbfac4ef08315eab61427d5cb11938
hurtstotouchfire <hurtstotouchfire@gmail.com>
2013-12-21 15:12:11 -0500

    versioning ssh config. keys still per machine, paths may  break.

--- setup.sh
+++ setup.sh
@@ -9,12 +9,20 @@ for file in $files; do
     ln -s $dir/$file ~/.$file 2> /dev/null
 done
 
-# this does not seem necessary. For now, a path (~/src...) in ~/.gitconfig to the versioned gitignore_global file seems to work. On golgi.
-# softlink git ignore
+# softlink ssh config
+echo "Pre-existing files in ~/.ssh:"
+ls ~/.ssh/
+# if directory or file exists, we will get some messages and the link will not trample anything
+# maybe someday this can graduate to some if statements
+mkdir ~/.ssh 2> /dev/null
+ln -s $dir/ssh_config ~/.ssh/config 2> /dev/null
+
+# This does not seem necessary anymore. 
+# For now, a path (~/src...) in ~/.gitconfig to the versioned gitignore_global file seems to work. On golgi and brick.
+## softlink git ignore
 #mkdir ~/.config 2> /dev/null
 #mkdir ~/.config/git 2> /dev/null
 #ln -s $dir/gitignore_global ~/.config/git/ignore 2> /dev/null
-
 
 # re-source bashrc
 . ~/.bashrc
